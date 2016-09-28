af90ef08d752b9da895aabb3796aa46e9ed0ce5e
jairoparedes <hiro.paredes.candia@gmail.com>
2016-09-28 12:56:22 -0300

    Integracion OSMDroid completa

--- app/src/main/java/inf/uct/nmicro/fragments/FragmentMapa.java
+++ app/src/main/java/inf/uct/nmicro/fragments/FragmentMapa.java
@@ -4,29 +4,41 @@ package inf.uct.nmicro.fragments;
  * Created by jairo on 9/28/2016.
  */
 import android.os.Bundle;
+import android.support.v4.BuildConfig;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
 import inf.uct.nmicro.R;
 
-public class FragmentMapa extends Fragment{
+public class FragmentMapa extends Fragment {
 
-        public FragmentMapa() {
-            // Required empty public constructor
-        }
+    public FragmentMapa() {
+        // Required empty public constructor
+    }
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_mapa, container, false);
-        }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //important! set your user agent to prevent getting banned from the osm servers
 
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_mapa, container, false);
+        org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+        MapView map = (MapView) rootView.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        return rootView;
+    }
+}
+
