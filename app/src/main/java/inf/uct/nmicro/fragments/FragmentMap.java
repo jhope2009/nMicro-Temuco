41df4cd89e322c833e1219781a72257086c61d1b
jairoparedes <hiro.paredes.candia@gmail.com>
2016-09-29 00:58:21 -0300

    Pocicionamiento en mapa, y posicionamiento actual del usuario

--- app/src/main/java/inf/uct/nmicro/fragments/FragmentMap.java
+++ app/src/main/java/inf/uct/nmicro/fragments/FragmentMap.java
@@ -12,11 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 import inf.uct.nmicro.R;
 
 public class FragmentMap extends Fragment {
 
+    private MapController   mapController;
     public FragmentMap() {
         // Required empty public constructor
     }
@@ -24,6 +29,7 @@ public class FragmentMap extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -36,6 +42,21 @@ public class FragmentMap extends Fragment {
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
+
+        mapController = (MapController) map.getController();
+        mapController.setZoom(14);
+        //-38.7392,-72.6087?z=14
+        GeoPoint Temuco = new GeoPoint(-38.7392, -72.6087);
+        mapController.setCenter(Temuco);
+        final MyLocationNewOverlay myLocationoverlay = new MyLocationNewOverlay(this.getActivity(), map);
+        myLocationoverlay.enableMyLocation();
+        map.getOverlays().add(myLocationoverlay); //No añadir si no quieres una marca
+
+        myLocationoverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo(myLocationoverlay.getMyLocation());
+            }
+        });
         return rootView;
     }
 }
